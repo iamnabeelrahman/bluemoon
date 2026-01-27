da1e277b4a3deaa4385d1976627eecb7d92c0924
Nabeel Rahman <mailnabeelrahman227@gmail.com>
2026-01-27 23:39:53 +0530

    update

--- components/footer.tsx
+++ components/footer.tsx
@@ -93,8 +93,14 @@ export function Footer() {
                 </div>
                 <div>
                   <p className="text-sm text-gray-400">Email</p>
-                  <p className="text-white">hello@bluemoon.com</p>
+                  <a
+                    href="mailto:info.bluemooninteriors@gmail.com"
+                    className="text-white hover:underline"
+                  >
+                    info.bluemooninteriors@gmail.com
+                  </a>
                 </div>
+
               </li>
               <li className="flex items-start gap-3">
                 <div className="rounded-lg bg-white/10 p-2">
@@ -102,14 +108,14 @@ export function Footer() {
                 </div>
                 <div>
                   <p className="text-sm text-gray-400">Location</p>
-                  <p className="text-white">Mumbai, Maharashtra, India</p>
+                  <p className="text-white">Delhi NCR, India</p>
                 </div>
               </li>
             </ul>
             <div className="flex items-center gap-3 mt-6">
-              <a href="#" className="rounded-lg bg-white/10 p-2 hover:bg-white/20 transition-colors">
+              {/* <a href="#" className="rounded-lg bg-white/10 p-2 hover:bg-white/20 transition-colors">
                 <Facebook className="w-5 h-5 text-white" />
-              </a>
+              </a> */}
               <a
                 href="https://www.instagram.com/blue.moon.interiors/"
                 target="_blank"
@@ -118,9 +124,9 @@ export function Footer() {
               >
                 <InstagramIcon className="w-5 h-5 text-white" />
               </a>
-              <a href="#" className="rounded-lg bg-white/10 p-2 hover:bg-white/20 transition-colors">
+              {/* <a href="#" className="rounded-lg bg-white/10 p-2 hover:bg-white/20 transition-colors">
                 <LinkedinIcon className="w-5 h-5 text-white" />
-              </a>
+              </a> */}
             </div>
           </div>
         </div>
